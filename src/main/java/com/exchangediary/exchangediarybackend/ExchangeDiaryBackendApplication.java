4dd92df76a0490e973229d08a0b6f8dbd6aaed0e
wjdah1028 <wjdah1028@naver.com>
2026-07-05 02:34:43 +0900

    :tada: Start: 교환일기 백엔드 Project 생성

---
+++ src/main/java/com/exchangediary/exchangediarybackend/ExchangeDiaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.exchangediary.exchangediarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeDiaryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeDiaryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exchangediary/exchangediarybackend/ExchangeDiaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchangediary.exchangediarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeDiaryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
