a3eba648cc0ed6ae2002a6ad67c2c33143c0bb51
wjdah1028 <wjdah1028@naver.com>
2026-07-07 10:30:59 +0900

    :recycle: Refactor: example 변경 및 config 파일 수정

--- src/main/java/com/exchangediary/exchangediarybackend/domain/user/dto/request/SignUpRequest.java
+++ src/main/java/com/exchangediary/exchangediarybackend/domain/user/dto/request/SignUpRequest.java
@@ -18,12 +18,12 @@ public class SignUpRequest {
     @NotBlank(message = "이름은 필수 입력값입니다.")
     private String userName;
 
-    @Schema(description = "이메일", example = "kkk02@naver.com")
+    @Schema(description = "이메일", example = "test@example.com")
     @NotBlank(message = "이메일은 필수 입력값입니다.")
     @Email(message = "이메일 형식에 맞지 않습니다.")
     private String email;
 
-    @Schema(description = "비밀번호", example = "c*123456789")
+    @Schema(description = "비밀번호", example = "password123!")
     @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}", message = "비밀번호는 영문 대,소문자, 숫자, 특수기호를 포함한 8~20자리여야 합니다.")
     private String password;
